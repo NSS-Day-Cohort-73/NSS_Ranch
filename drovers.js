ab18758c2576dd4af6390484606270a080ad83c8
Logan Sewell <47334784+LoganSewell@users.noreply.github.com>
2024-09-26 09:05:26 -0500

    Update drovers.js

--- drovers.js
+++ drovers.js
@@ -1,14 +1,24 @@
 const { database } = require("./database.js")
 
+// Creates a new function named hireDrovers.
 const hireDrovers = (herdSize) => {
-    const drovers = []
-    const allDrovers = database.drovers
-    const numberNeeded = cattleToDrive / 10
+// Sets herdSize equal to the value of hireDrovers.
 
+// Creates a new empty array of drovers.
+    const drovers = []
+// Tells drovers to look in the database module for allDrovers.
+    const allDrovers = database.drovers
+// Creating new variable numberNeeded to equal herdSize divided by 10.
+    const numberNeeded = herdSize / 10
+
+// This loop is counting from 0 up to numberNeeded - 1 and repeating an action each time. It helps you run code multiple times without needing to write the same thing over and over again.
     for (let counter = 0; counter < numberNeeded; counter++) {
+// This code generates a random index based on the length of the allDrovers array and assigns it to the variable randomHerderId.
         const randomHerderId = Math.floor(Math.random() * allDrovers.length)
+// This line selects a random element from the allDrovers array (using the index randomHerderId) and adds it to the drovers array by pushing it to the end of that array. The result is that drovers is expanded with one more element from allDrovers.
         drovers.push(allDrovers[randomHerderId])
     }
+    return drovers
 
 }
 
